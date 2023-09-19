b72e3f42969422763d5e0fe15b65818e0ecd652f
Sperandio.20200 <Sperandio.20200@E2-9-PC12>
2023-09-19 13:04:54 +0200

    Aggiungere i file di progetto.

---
+++ es.1 classe prodotto/Program.cs
@@ -0,0 +1,24 @@
+﻿using es._1_classe_prodotto.Properties;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es._1_classe_prodotto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nm = "pane";
+            int pr = 2;
+            prodotto p = new prodotto(nm, pr);
+            prodotto n = new prodotto(nm, pr);
+            Console.WriteLine(p.Nome());
+            Console.WriteLine(n.Prezzo());
+        }
+    }
+}
---
+++ es.1 classe prodotto/Properties/Class1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es._1_classe_prodotto.Properties
+{
+    //creazione classe prodotto
+    public class prodotto
+    {
+        private string nome;
+        private int prezzo;
+
+        //costruttore parametri+
+        public prodotto(string nome1, int prezzo1)
+        {
+            nome = nome1;
+            prezzo= prezzo1;
+        }
+
+        //metodi che restituiscano il valore degli attributi
+        public string Nome()
+        {
+            return nome;
+        }
+        public int Prezzo()
+        {
+            return prezzo;
+        }
+    } 
+}
